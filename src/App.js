dc6ba9919d47fcf16f6a4b84d15b90d96ab02656
AlishanMasood <alishanmasood20@gmail.com>
2024-10-22 03:06:29 +0500

    added landing page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import AboutUs from "./components/AboutUs";
-import Categories from "./components/Categories";
-import ClientTestmonials from "./components/ClientTestmonials";
-import ContactUS from "./components/ContactUs";
-import Features from "./components/Features";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import PartnerCompanies from "./components/PartnerCompanies";
-import ProductList from "./components/ProductList";
-import Promotions from "./components/Promotions";
+import LandingPage from "./pages/LandingPage";
 function App() {
   return (
     <div className="App">
-      <Header />
-
-      <Features />
-      <ProductList />
-      <Categories />
-      <Promotions />
-      <AboutUs />
-      <PartnerCompanies />
-      <ClientTestmonials />
-      <ContactUS />
-      <Footer />
+      <LandingPage />
     </div>
   );
 }
