c7c60a4a252fb5c1695c6df172b8570f1c01c929
AlishanMasood <alishanmasood20@gmail.com>
2024-11-09 20:39:52 +0500

    routes are created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import SingleProductPage from "./pages/SingleProductPage";
+import SubCategoriesPage from "./pages/SubCategoriesPage";
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/categorie/:subcategory" element={<SubCategoriesPage />} />
+      </Routes>
+
+      {/* <SingleProductPage /> */}
+      {/* <LandingPage /> */}
+    </BrowserRouter>
   );
 }
 
