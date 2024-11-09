458277d6069f86b4175c3dd6189d0f61bb9a64ae
AlishanMasood <alishanmasood20@gmail.com>
2024-11-09 20:40:39 +0500

    data organized

--- src/components/constants.js
+++ src/components/constants.js
@@ -31,3 +31,251 @@ export const features = [
     icon: FingerPrintIcon,
   },
 ];
+export const products = [
+  {
+    id: 1,
+    name: "Ceramic Tile",
+    subcategory: "#",
+    imageSrc:
+      "https://plus.unsplash.com/premium_photo-1675747693497-0a1d0db0740c?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 2,
+    name: "white ceramic bowl",
+    subcategory: "#",
+    imageSrc:
+      "https://images.unsplash.com/photo-1595514535116-d0401260e7cf?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 3,
+    name: "Carved Mirror",
+    subcategory: "#",
+    imageSrc:
+      "https://plus.unsplash.com/premium_photo-1677058585435-6eb9bd120b15?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 3,
+    name: "Compost Toilet",
+    subcategory: "#",
+    imageSrc:
+      "https://plus.unsplash.com/premium_photo-1664392164625-4f806c5f70f5?q=80&w=1651&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  {
+    id: 3,
+    name: "Steel shower set",
+    subcategory: "#",
+    imageSrc:
+      "https://images.unsplash.com/photo-1576678433413-202829a1ab98?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$35",
+    color: "Black",
+  },
+  // More products...
+];
+
+export const callouts = [
+  {
+    name: "Tiles",
+
+    imageSrc:
+      "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+    imageAlt:
+      "Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.",
+    subcategory: "tiles",
+    subCategories: [
+      {
+        name: "Washroom",
+        id: "wall-tiles",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+      {
+        name: "Kitchen",
+        id: "kitchen",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+      {
+        name: "flooring",
+        id: "flooring",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+    ],
+  },
+  {
+    name: "Stone Tiles",
+
+    imageSrc:
+      "https://img.freepik.com/free-photo/light-red-brick-wall_169016-3239.jpg?t=st=1729546698~exp=1729550298~hmac=21670955934fd3816f8fbf1a27b0145aa474a4f3a379c493545f7981d280c2df&w=1060",
+    imageAlt:
+      "Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.",
+    subcategory: "Stone-Tiles",
+    subCategories: [
+      {
+        name: "Random",
+        id: "random",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+      {
+        name: "Singhada",
+        id: "singhada",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+      {
+        name: "Split",
+        id: "split",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+    ],
+  },
+  {
+    name: "Sanitary Ware",
+
+    imageSrc:
+      "https://img.freepik.com/free-photo/small-bathroom-with-modern-style-plants_23-2150700372.jpg?t=st=1729546763~exp=1729550363~hmac=3a45dcc58a998fa138b4c6157ee188e8100e5c29ad90a6e6e5743bc1bc88de8c&w=826",
+    imageAlt: "Collection of four insulated travel bottles on wooden shelf.",
+    subcategory: "Sanitary-Ware",
+    subCategories: [
+      {
+        name: "Commode",
+        id: "commode",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+      {
+        name: "Indian Toilet",
+        id: "indian-toilet",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+      {
+        name: "Wash Basin",
+        id: "wash-basin",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+      {
+        name: "Bowls",
+        id: "bowls",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+    ],
+  },
+  {
+    name: "Sanitary Fittings",
+
+    imageSrc:
+      "https://img.freepik.com/premium-photo/faucet-that-is-made-by-faucet_811830-10872.jpg?w=740",
+    imageAlt: "Collection of four insulated travel bottles on wooden shelf.",
+    subcategory: "Sanitary-Fittings",
+    subCategories: [
+      {
+        name: "Shower-Sets",
+        id: "Shower-Sets",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+      {
+        name: "Vanity Mixers",
+        id: "Vanity-Mixers",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+      {
+        name: "Basin Mixers",
+        id: "basin-mixers",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+    ],
+  },
+  {
+    name: "Vanities",
+
+    imageSrc:
+      "https://img.freepik.com/free-photo/home-interior-design-composition_23-2148986633.jpg?t=st=1729546957~exp=1729550557~hmac=1abf311313ce3785fd5af914ab80921056c3659b510e601878b3369f74e2ff09&w=826",
+    imageAlt: "Collection of four insulated travel bottles on wooden shelf.",
+    subcategory: "Vanities",
+    subCategories: [
+      {
+        name: "PVC-Vanity",
+        id: "PVC-Vanity",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+    ],
+  },
+  {
+    name: "Kitchen Items",
+
+    imageSrc:
+      "https://img.freepik.com/premium-photo/modern-stylish-washbasin-perfect-contemporary-bathroom-design_1142-213347.jpg?w=740",
+    imageAlt: "Collection of four insulated travel bottles on wooden shelf.",
+    subcategory: "Kitchen-Items",
+    subCategories: [
+      {
+        name: "Sink-Bowls",
+        id: "Sink-Bowls",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+      {
+        name: "Sink Mixers",
+        id: "Sink-Mixers",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+      {
+        name: "Soap Dispensers",
+        id: "Soap-Dispensers",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+    ],
+  },
+  {
+    name: "Plastic Items",
+
+    imageSrc:
+      "https://img.freepik.com/free-photo/construction-pvc-pipes-composition_23-2149106876.jpg?t=st=1729547229~exp=1729550829~hmac=325dfdfd892b4ff52e03e9622711f1f4e73940c1f3412d85cbdee627bb7d8522&w=826",
+    imageAlt: "Collection of four insulated travel bottles on wooden shelf.",
+    subcategory: "Plastic-Items",
+    subCategories: [
+      {
+        name: "Floor-Drain",
+        id: "Floor-Drain",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+      {
+        name: "Pipes",
+        id: "pipes",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+      {
+        name: "Mirrors",
+        id: "mirrors",
+        imageSrc:
+          "https://img.freepik.com/free-vector/colored-realistic-ceramic-floor-tiles-horizontal-set-with-squares-different-types-styles-tiles_1284-29013.jpg?t=st=1729546626~exp=1729550226~hmac=f6a4023c38bae2df421df994851ebace2386223f3b7fd0ff15c9699aa62a936f&w=740",
+      },
+    ],
+  },
+];
